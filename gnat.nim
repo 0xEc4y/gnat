b668ea548def6f2178575a0b87ae5d7b99ee3862
0xEc4y <etstarks@proton.me>
2023-12-13 17:30:33 -0800

    commit

--- gnat.nim
+++ gnat.nim
@@ -1,16 +1,16 @@
 import os, strformat
 
 
-# Displays the file buffer which was handled by the openFilePrintContents() function
+# Displays the file buffer which was handled by the checkFileExists() function
 proc displayContents(buffer: string) =
-  # Currently writes out file contents
+  # Currently only writes out file contents
 
   for i in 0 ..< buffer.len:
     stdout.write buffer[i]
 
 
-# Opens passed file and takes in all contents as string. Will pass to helper function to handle printing.
-proc openFilePrintContents(fileName: string): seq[string] =
+# Opens passed file and takes in all contents as string. Will pass to helper function to handle output.
+proc checkFileExists(fileName: string): seq[string] =  # Intended to return nothing, set to return sequence of strings to understand "discard" logic.
   if fileExists(fileName) == false:
     echo &"\n\nFile does not exist.. Quitting.\n\n"
     quit()
@@ -30,14 +30,16 @@ proc openFilePrintContents(fileName: string): seq[string] =
 # Equivlant to "if __name__ == '__main__'"
 when isMainModule:
   let args = commandLineParams()
-  setControlCHook(proc() {.noconv.} = quit())
-
+  setControlCHook(proc() {.noconv.} = quit())   # Quit when CTRL+C is pressed
 
   if args.len == 0:
-    echo &"\n\ndid not recieve a file for input.\n\n"
-    quit()
+    echo &"\n\nReturning standard input. (CTRL+C to quit).\n"
+    while true:
+      let word = readLine(stdin)
+      stdout.write word
+      echo "\n"
   elif args.len == 1:
-    discard openFilePrintContents(paramStr(1)) #  call function and discard required returned value
+    discard checkFileExists(paramStr(1)) #  call function and discard required returned value
   elif args.len >= 2:
     echo &"\n\ninvalid number of arguments, please run and only pass one argument (the file to be read).\n\n"
     quit()
